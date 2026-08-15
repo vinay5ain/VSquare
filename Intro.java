c735558730e8ab7bdcf889007abf2f945a337156
Vidit2504 <viditjain2504@gmail.com>
2026-08-15 18:31:51 +0530

    I  vidit

---
+++ Intro.java
@@ -0,0 +1,10 @@
+// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
+public class Intro {
+   public Intro() {
+   }
+
+   public static void main(String[] var0) {
+      System.out.println("Hello, World!");
+   }
+}
+ 
